82046626916400a117a09a9406df176b7940cb60
marianamaher <mariana.maherr@gmail.com>
2023-10-13 12:05:28 -0400

    added process.env instead of reference to envs/

--- src/app/auth/api-config.service.ts
+++ src/app/auth/api-config.service.ts
@@ -1,13 +1,12 @@
 // src/app/config.service.ts
 
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfigService {
   getApiKey(): string {
-    return environment.firebaseAPIKey;
+    return process.env.firebaseAPIKey;
   }
 }
