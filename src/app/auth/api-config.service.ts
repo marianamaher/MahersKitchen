b1d33995cf346635fa38d38733f64c52b2a2052c
marianamaher <mariana.maherr@gmail.com>
2023-10-13 13:22:08 -0400

    changing apiConfig back

--- src/app/auth/api-config.service.ts
+++ src/app/auth/api-config.service.ts
@@ -1,26 +1,13 @@
 // src/app/config.service.ts
 
 import { Injectable } from '@angular/core';
-
-
-declare const process: {
-    env: {
-      NODE_ENV: string;
-      firebaseAPIKey: string;
-    };
-  };
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfigService {
-  getApiKey(){
-    return {
-        production: process.env.NODE_ENV === 'production',
-        apiKey: process.env.firebaseAPIKey || '',
-        // Add other environment-specific variables as needed
-      };
-    // return (window as any).process?.env.firebaseAPIKey;
-    // return process.env.firebaseAPIKey;
+  getApiKey(): string {
+    return environment.firebaseAPIKey;
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
   private apiKey: string;
 
   constructor(private http: HttpClient, private router: Router, private apiService: ConfigService) {
-    this.apiKey = this.apiService.getApiKey().apiKey;
+    this.apiKey = this.apiService.getApiKey();
    }
   
 
