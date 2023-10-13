063f696ad7463383eec342ce3b29f7ea60d3ac01
marianamaher <mariana.maherr@gmail.com>
2023-10-13 12:46:18 -0400

    created .env file

--- src/app/auth/api-config.service.ts
+++ src/app/auth/api-config.service.ts
@@ -2,12 +2,25 @@
 
 import { Injectable } from '@angular/core';
 
+
+declare const process: {
+    env: {
+      NODE_ENV: string;
+      firebaseAPIKey: string;
+    };
+  };
+
 @Injectable({
   providedIn: 'root',
 })
 export class ConfigService {
-  getApiKey(): string {
-    return (window as any).process?.env.firebaseAPIKey;
+  getApiKey(){
+    return {
+        production: process.env.NODE_ENV === 'production',
+        apiKey: process.env.firebaseAPIKey || '',
+        // Add other environment-specific variables as needed
+      };
+    // return (window as any).process?.env.firebaseAPIKey;
     // return process.env.firebaseAPIKey;
   }
 }
