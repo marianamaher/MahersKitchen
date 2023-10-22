8e42c4de0c4652d5aa28b02617f1a5db59c96b6d
marianamaher <mariana.maherr@gmail.com>
2023-10-21 22:49:11 -0400

    changing styles + logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Veganista CB';
+  title ="Maher's Kitchen";
 
   constructor(private authService: AuthService){}
 
